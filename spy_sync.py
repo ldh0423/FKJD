7b5353d88d505365ea4ecc94b0060da6cb4092b1
ldh0423 <liudengh0554@foxmail.com>
2024-08-01 15:30:56 +0800

    sync

--- spy_sync.py
+++ spy_sync.py
@@ -1,9 +1,8 @@
 import os
 import shutil
 import requests
-import re
+import yaml
 
-# Step 1: 从网站中抓取内容
 url = 'https://raw.githubusercontent.com/shufflewzc/AutoSpy/master/config/Faker.spy'  # 请替换为目标网站的URL
 response = requests.get(url)
 if response.status_code == 200:
@@ -11,16 +10,14 @@ if response.status_code == 200:
 else:
     raise Exception(f"Failed to fetch content from {url}")
 
-# Step 2: 在抓取的内容中使用正则表达式提取 `Script:` 后的文件名
-pattern = re.compile(r'Script: \s*(\w+\.js)')
-matched_files = pattern.findall(content)
+data = yaml.safe_load(content)
 
+source_folder = '../faker3/'
+destination_folder = './'
 
-# Step 3: 检查指定文件夹中是否有这些文件，并复制到目标文件夹
-source_folder = '../faker3/'  # 请替换为源文件夹路径
-destination_folder = './'  # 请替换为目标文件夹路径
+for Container in data["js_config"]:
 
-for file_name in matched_files:
+    file_name = Container["Script"]
     source_file_path = os.path.join(source_folder, file_name)
     destination_file_path = os.path.join(destination_folder, file_name)
 
@@ -29,3 +26,8 @@ for file_name in matched_files:
         print(f"文件 {file_name} 已成功复制到 {destination_folder}")
     else:
         print(f"文件 {file_name} 在 {source_folder} 中不存在")
+
+    Container["Script"] = "ldh0423_FKJD/" + Container["Script"]
+
+with open('Faker.spy', 'w', encoding='utf-8') as file:
+    yaml.dump(data, file, allow_unicode=True, default_flow_style=False)
